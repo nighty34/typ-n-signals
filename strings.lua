6003a6a248567958ea01566082c011da884c679b
Felix nightfury <mc.nightfury34@gmail.com>
2024-04-14 00:16:22 +0200

    add strings.lua

---
+++ strings.lua
@@ -0,0 +1,44 @@
+﻿function data()
+return {
+	de = {
+		["nighty_no"] = ("Aus"),
+		["nighty_yes"] = ("An"),
+
+		["nighty_type_n_signaltype"] = ("Signaltyp"),
+
+		["nighty_signal_normal"] = ("Standard"),
+		["nighty_signal_entry"] = ("Einfahrtssignal"),
+		["nighty_signal_exit"] = ("Ausfahrtssignal"),
+
+
+		["nighty_type_n_speedindicator"] = ("Geschwindigkeitsanzeige"),
+		["nighty_type_n_indicatesnextspeed"] = ("Geschwindigkeit ist Voranzeige"),
+
+		["nighty_type_n_hauptsignal_name"] = ("Typ N Hauptsignal"),
+		["nighty_type_n_hauptsignal_desc"] = ("Hauptsignale des Typ N Systems mit oder ohne Geschwindigkeitsanzeige"),
+
+		["nighty_type_n_vorsignal_name"] = ("Typ N Vorsignal"),
+		["nighty_type_n_vorsignal_desc"] = ("Vorsignale des Typ N Systems mit oder ohne Geschwindigkeitsanzeige"),
+	},
+	en = {
+		["nighty_no"] = ("Off"),
+		["nighty_yes"] = ("On"),
+
+		["nighty_type_n_signaltype"] = ("Signal Type"),
+
+		["nighty_signal_normal"] = ("Standard"),
+		["nighty_signal_entry"] = ("Entry Signal"),
+		["nighty_signal_exit"] = ("Exit Signal"),
+
+
+		["nighty_type_n_speedindicator"] = ("Speed Indicator"),
+		["nighty_type_n_indicatesnextspeed"] = ("Speed Indicator is Preindicating"),
+
+		["nighty_type_n_hauptsignal_name"] = ("Type N Main Signal"),
+		["nighty_type_n_hauptsignal_desc"] = ("Minsignal from the Type N System with or without Speedindicator"),
+
+		["nighty_type_n_vorsignal_name"] = ("Type N Presignal"),
+		["nighty_type_n_vorsignal_desc"] = ("Presignal from the Type N System with or without Speedindicator"),
+	}
+}
+end
