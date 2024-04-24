a1e631e6cc8b2bb9773c455d8977ee693b743afc
Felix nightfury <mc.nightfury34@gmail.com>
2024-04-24 02:25:27 +0200

    add missing strings

--- strings.lua
+++ strings.lua
@@ -1,44 +1,60 @@
 ﻿function data()
 return {
 	de = {
+		["nighty_type_n_hauptsignal_name"] = ("Typ N Hauptsignal"),
+		["nighty_type_n_hauptsignal_desc"] = ("Hauptsignale des Typ N Systems mit oder ohne Geschwindigkeitsanzeige"),
+
 		["nighty_no"] = ("Aus"),
 		["nighty_yes"] = ("An"),
 
 		["nighty_type_n_signaltype"] = ("Signaltyp"),
-
-		["nighty_signal_normal"] = ("Standard"),
-		["nighty_signal_entry"] = ("Einfahrtssignal"),
-		["nighty_signal_exit"] = ("Ausfahrtssignal"),
-
+		["nighty_type_n_signaltype_tooltip"] = ("Wähle einen Signaltyp (Einfahrt-/Ausfahrt- oder Vorangezeigte Einfahrt)"),
 
 		["nighty_type_n_speedindicator"] = ("Geschwindigkeitsanzeige"),
-		["nighty_type_n_indicatesnextspeed"] = ("Geschwindigkeit ist Voranzeige"),
+		["nighty_type_n_speedindicator_tooltip"] = ("Wähle ob das Signal mit Geschwindigkeitsanzeige gebaut werden soll."),
 
-		["nighty_type_n_hauptsignal_name"] = ("Typ N Hauptsignal"),
-		["nighty_type_n_hauptsignal_desc"] = ("Hauptsignale des Typ N Systems mit oder ohne Geschwindigkeitsanzeige"),
+		["nighty_type_n_shorten_block"] = ("Verkürzte Blockabstände"),
+		["nighty_type_n_shorten_block_tooltip"] = ("[Geschwindigkeitsanzeige muss aktiv sein] Wähle, ob das Signal das Signalbild 'Warnung Verkürzter Blockabstand' datstellen soll."),
+
+		["nighty_type_n_occupied"] = ("Block ist belegt"),
+		["nighty_type_n_occupied_tooltip"] = ("[Geschwindigkeitsanzeige muss aktiv sein] Wähle, ob das Signal das Signalbild 'Belegtes Gleis' datstellen soll."),
 
 		["nighty_type_n_vorsignal_name"] = ("Typ N Vorsignal"),
 		["nighty_type_n_vorsignal_desc"] = ("Vorsignale des Typ N Systems mit oder ohne Geschwindigkeitsanzeige"),
+
+		["nighty_type_n_pre_station"] = ("Stationeinfahrts Vorankündigung"),
+		["nighty_type_n_pre_station_tooltip"] = ("Wähle, ob das Signal ein Zusatzschild als Vorankündigung für ein Einfahrtssignal darstellen soll"),
+
+		["nighty_invisible_signal"] = ("Unsichtbares Signal"),
+
+
 	},
 	en = {
 		["nighty_no"] = ("Off"),
 		["nighty_yes"] = ("On"),
 
-		["nighty_type_n_signaltype"] = ("Signal Type"),
-
-		["nighty_signal_normal"] = ("Standard"),
-		["nighty_signal_entry"] = ("Entry Signal"),
-		["nighty_signal_exit"] = ("Exit Signal"),
-
-
-		["nighty_type_n_speedindicator"] = ("Speed Indicator"),
-		["nighty_type_n_indicatesnextspeed"] = ("Speed Indicator is Preindicating"),
-
 		["nighty_type_n_hauptsignal_name"] = ("Type N Main Signal"),
 		["nighty_type_n_hauptsignal_desc"] = ("Minsignal from the Type N System with or without Speedindicator"),
 
+		["nighty_type_n_signaltype"] = ("Signal Type"),
+		["nighty_type_n_signaltype_tooltip"] = ("Choose a signaltype (Entry-/Exit- or Entry Announcement)"),
+
+		["nighty_type_n_speedindicator"] = ("Speed Indicator"),
+		["nighty_type_n_speedindicator_tooltip"] = ("Choose if the signal should have a speed indicator."),
+
+		["nighty_type_n_shorten_block"] = ("Shortened Block"),
+		["nighty_type_n_shorten_block_tooltip"] = ("[Needs Speedindicator to be active] Choose if the signal should be able to display 'Warning, Shortened Blockdistance'."),
+
+		["nighty_type_n_occupied"] = ("Track is Occupied"),
+		["nighty_type_n_occupied_tooltip"] = ("[Needs Speedindicator to be active] Choose if the signal should be able to display 'Track is occupied'."),
+
 		["nighty_type_n_vorsignal_name"] = ("Type N Presignal"),
 		["nighty_type_n_vorsignal_desc"] = ("Presignal from the Type N System with or without Speedindicator"),
+
+		["nighty_type_n_pre_station"] = ("Pre-Entry Sign"),
+		["nighty_type_n_pre_station_tooltip"] = ("Choose if the 'Pre-Entry Sign' should be displayed"),
+
+		["nighty_invisible_signal"] = ("Invisible Signal"),
 	}
 }
 end
