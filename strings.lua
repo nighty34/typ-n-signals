877d9a20a3767f7aa24286536b31f23f8c7597cd
nightfury34 <mc.nightfury34@gmail.com>
2024-05-10 20:43:06 +0200

    add z-offset

--- strings.lua
+++ strings.lua
@@ -7,7 +7,7 @@ return {
 		["nighty_no"] = ("Aus"),
 		["nighty_yes"] = ("An"),
 
-		["nighty_type_n_signaltype"] = ("Signaltyp"),
+		["nighty_type_n_signaltype"] = ("Signaltyp\nText kann im Signal-Namen geändert werden.\n z.B. [Zue] oder [Zue|ZHdb] für mehrere Schilder."),
 		["nighty_type_n_signaltype_tooltip"] = ("Wähle einen Signaltyp (Einfahrt-/Ausfahrt- oder Vorangezeigte Einfahrt)"),
 
 		["nighty_type_n_speedindicator"] = ("Geschwindigkeitsanzeige"),
@@ -19,7 +19,7 @@ return {
 		["nighty_type_n_occupied"] = ("Block ist belegt"),
 		["nighty_type_n_occupied_tooltip"] = ("[Geschwindigkeitsanzeige muss aktiv sein] Wähle, ob das Signal das Signalbild 'Belegtes Gleis' datstellen soll."),
 
-		["nighty_type_n_id_sign"] = ("Signal Nummer (E34)"),
+		["nighty_type_n_id_sign"] = ("Signal Nummer\nText kann im Signal-Namen geändert werden.\nz.B. (E34)"),
 		["nighty_type_n_id_sign_tooltip"] = ("Wähle, ob das Signal ein kleines Schild auf dem die Signalnummer angezeigt wird, haben soll."),
 
 		["nighty_type_n_vorsignal_name"] = ("Typ N Vorsignal"),
@@ -34,13 +34,26 @@ return {
 		["nighty_invisible_signal"] = ("Unsichtbares Signal"),
 		["nighty_invisible_waypoint"] = ("Unsichtbarer Wegpunkt"),
 
+		["nighty_type_n_build_form"] = ("Signal Bauform"),
+		["nighty_type_n_build_form_old"] = ("Alt"),
+		["nighty_type_n_build_form_brugg"] = ("Brugg"),
+		["nighty_type_n_build_form_rappi"] = ("Rappi"),
+
+		["nighty_type_n_mast"] = ("Signal Mast"),
+		["nighty_type_n_mast_basic"] = ("Standard"),
+		["nighty_type_n_mast_offset"] = ("Versetzt"),
+
 		["sbb_type_n_name"] = ("SBB Typ N Signale (Better Signals)"),
 		["sbb_type_n_name_desc"] = ("Schweizer Signale vom Typ N. Im Paket enthalten sind sowohl Vor- wie auch Hauptsignale. Die Signale sind Teil des [Better Signals] Projekts"),
 
 		["better_signals_view_distance"] = ("Signal Erkennungsdistanz"),
 		["better_signals_view_distance_tooltip"] = ("Stelle ein, wie weit ein Zug Signale sehen kann"),
 
-
+		["better_signals_x_offset"] = ("Signalauswahl zum verknüpfen kann \nmit ü und ¨ geändert werden.\n\nSignal Versatz"),
+		["better_signals_x_offset_tooltip"] = ("Wähle auf welcher Seite vom Gleis, das Signal snappen soll."),
+		["better_signals_snap_left"] = ("Links"),
+		["better_signals_snap_right"] = ("Rechts"),
+		["better_signals_snap_middle"] = ("Mitte"),
 	},
 	en = {
 		["nighty_no"] = ("Off"),
